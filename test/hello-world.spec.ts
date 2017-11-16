dd13e5dfb4cf2f7578cddd1cbf1644f5c54d9307
Saeed Ganji <shahabganji@gmail.com>
2017-11-16 22:55:14 +0330

    test(hello-world component)

--- test/hello-world.spec.ts
+++ test/hello-world.spec.ts
@@ -7,13 +7,13 @@ describe('HelloWorld', () => {
   beforeEach(() => {
     component = StageComponent
       .withResources('hello-world')
-      .inView('<span class="hello-world">${msg}</span>')
+      .inView('<span>${msg}</span>')
       .boundTo({ msg: 'Hello Aurelia Plugins' });
   });
 
   it('should render first name', done => {
     component.create(bootstrap).then(() => {
-      const nameElement = document.querySelector('.hello-world');
+      const nameElement = document.querySelector('span');
       expect(nameElement.innerHTML).toBe('Hello Aurelia Plugins');
       done();
     }).catch(e => { console.log(e.toString()); });
